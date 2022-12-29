0a7429c5065dd9a8250b2da1bd2533915b337eed
Marek Kalus <93349057+Marektg@users.noreply.github.com>
2022-12-29 18:50:06 +0100

    up

--- myApp.js
+++ myApp.js
@@ -1,6 +1,10 @@
 let express = require('express');
 let app = express();
-console.log("Hello World")
+console.log("Hello World");
+
+app.get("/", function (req, res) {
+    res.send("Hello Express");
+});
 
 
 
