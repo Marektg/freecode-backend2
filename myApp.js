c7baeeb64bc4d70a379f44d6b8b7a43bbe996dc9
Marek Kalus <93349057+Marektg@users.noreply.github.com>
2022-12-31 18:45:32 +0100

    add static resource

--- myApp.js
+++ myApp.js
@@ -1,11 +1,13 @@
 let express = require('express');
 let app = express();
 // console.log("Hello World");
-let filePath = __dirname + "/views/index.html"
+let filePath = __dirname + "/views/index.html";
+let publicPath = __dirname + "/public";
 app.get("/", function (req, res) {
 
     res.sendFile(filePath)
 });
+app.use("/public", express.static(publicPath) )
 
 
 
