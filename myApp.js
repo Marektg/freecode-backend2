7308f53aaaf607a63546a52ed31c2073b104816f
Marek Kalus <93349057+Marektg@users.noreply.github.com>
2022-12-31 18:18:52 +0100

    up

--- myApp.js
+++ myApp.js
@@ -3,7 +3,8 @@ let app = express();
 console.log("Hello World");
 
 app.get("/", function (req, res) {
-    res.send("Hello Express");
+    // res.send("Hello Express");
+    res.sendFile(__dirname +"/views/index.html")
 });
 
 
