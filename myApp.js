c6fa760a6f9accec23171a74b813590dec7d6bc3
Marek Kalus <93349057+Marektg@users.noreply.github.com>
2022-12-31 18:28:09 +0100

    update

--- myApp.js
+++ myApp.js
@@ -1,10 +1,10 @@
 let express = require('express');
 let app = express();
-console.log("Hello World");
-
+// console.log("Hello World");
+let filePath = __dirname + "/views/index.html"
 app.get("/", function (req, res) {
 
-    res.sendFile(__dirname +"/views/index.html")
+    res.sendFile(filePath)
 });
 
 
