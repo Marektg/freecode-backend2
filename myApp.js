84db6322404e97105470eca3cda322221817db81
Marek Kalus <93349057+Marektg@users.noreply.github.com>
2022-12-31 19:39:20 +0100

    add env

--- myApp.js
+++ myApp.js
@@ -1,17 +1,28 @@
 let express = require('express');
+let variable = require('dotenv').config();
 let app = express();
 // console.log("Hello World");
 let filePath = __dirname + "/views/index.html";
 let publicPath = __dirname + "/public";
+console.log(variable.parsed.MESSAGE_STYLE);
 app.get("/", function (req, res) {
 
     res.sendFile(filePath)
 });
 app.use("/public", express.static(publicPath));
-app.get("/json", function (reg, res) {
+app.get("/json", function (req, res) {
+    let message = "Hello json";
+    if (variable.parsed.MESSAGE_STYLE === "uppercase") {
+        newMessage = message.toUpperCase();
+    
+        res.json({
+            message: `${newMessage}`
+        });
+    } else {
     res.json({
-        message: "Hello json"
+        message: `${message}`
     });
+}
 })
 
 
